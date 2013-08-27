a26947ec50ff85e551fbc40c51cff2ca3d41121f
Will Hawkins <hawkinsw@opentechinstitute.org>
2013-08-27 15:00:44 -0400

    Fix bug (iptoint before lookup)

--- hops.py
+++ hops.py
@@ -27,14 +27,15 @@ def as_array(filename,skip=1):
   return ases
 
 def lookup_as(ip, ases, cache):
-  if (ip in cache):
+  if (cache and ip in cache):
     return cache[ip]
   for as_and_range in ases:
     s = ases[as_and_range].split(',')
     low = int(s[1])
     high = int(s[2])
     if (low <= ip and ip <= high):
-      cache[ip] = s[0]
+      if cache:
+        cache[ip] = s[0]
       return s[0]
   return "NO AS"
 
@@ -86,8 +87,8 @@ def asify_hop_array(hops, ases):
   for hop_a in hops:
     for hop_b in hops[hop_a]:
       print "Hop pair ..."
-      as_hop_a = lookup_as(hop_a, ases, as_cache)
-      as_hop_b = lookup_as(hop_b, ases, as_cache)
+      as_hop_a = lookup_as(iptoint(hop_a), ases, as_cache)
+      as_hop_b = lookup_as(iptoint(hop_b), ases, as_cache)
       if as_hop_a not in as_hops:
         as_hops[as_hop_a] = {}
       if as_hop_b not in as_hops[as_hop_a]:
@@ -112,3 +113,4 @@ ases = as_array("GeoIPASNum2.csv", 0)
 hops = hop_array("cache/stage3.comcast.lga01.sql.csv", rate)
 as_hops = asify_hop_array(hops, ases)
 write_hop_array("cache/hops.csv", as_hops)
+#print lookup_as(iptoint("8.8.8.8"), ases, None)
