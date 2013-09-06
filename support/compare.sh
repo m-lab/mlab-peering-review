f32f2516077ca374cccec71a3a03061d32f74cea
Stephen Soltesz <stephen.soltesz@gmail.com>
2013-09-05 21:35:19 -0400

    add clearer labels

--- support/compare.sh
+++ support/compare.sh
@@ -4,11 +4,13 @@ PREFIX1=${1:?HELP: First Prefix name, such as city.}
 SITE1=${2:?HELP: First M-Lab site name, i.e. lga01, lax01, etc.}
 ISP1=${3:?HELP: First short ISP name, i.e. warner, comcast, verizon}
 COLOR1=${4:?HELP: First color, i.e. red, blue, green, etc.}
+PROVIDER1=${5:?HELP: First site provider}
 
-PREFIX2=${5:?HELP: Second Prefix name, such as city.}
-SITE2=${6:?HELP: Second M-Lab site name, i.e. lga01, lax01, etc.}
-ISP2=${7:?HELP: Second short ISP name, i.e. warner, comcast, verizon}
-COLOR2=${8:?HELP: Second color, i.e. red, blue, green, etc.}
+PREFIX2=${6:?HELP: Second Prefix name, such as city.}
+SITE2=${7:?HELP: Second M-Lab site name, i.e. lga01, lax01, etc.}
+ISP2=${8:?HELP: Second short ISP name, i.e. warner, comcast, verizon}
+COLOR2=${9:?HELP: Second color, i.e. red, blue, green, etc.}
+PROVIDER2=${10:?HELP: Second site provider.}
 
 export TZ=UTC
 
@@ -18,15 +20,19 @@ set -e
 # RAW SAMPLES
 PREFIX=$PREFIX1
 if test "$PREFIX1" != "$PREFIX2" ; then
-    PREFIX=$PREFIX1.$PREFIX2
+    PREFIX=${PREFIX1}_vs_$PREFIX2
 fi
 SITE=$SITE1
 if test "$SITE1" != "$SITE2" ; then
-    SITE=$SITE1.$SITE2
+    SITE=${SITE1}_vs_$SITE2
 fi
 ISP=$ISP1
 if test "$ISP1" != "$ISP2" ; then
-    ISP=$ISP1.$ISP2
+    ISP=${ISP1}_vs_$ISP2
+fi
+PROVIDER=$PROVIDER1
+if test "$PROVIDER1" != "$PROVIDER2" ; then
+    PROVIDER=${PROVIDER1}_vs_$PROVIDER2
 fi
 
 file1=cache/stage1.$PREFIX1.$SITE1.$ISP1.sql.csv
@@ -35,8 +41,8 @@ tmp1=tmp/stage1.$PREFIX1.$SITE1.$ISP1.sql.csv
 tmp2=tmp/stage1.$PREFIX2.$SITE2.$ISP2.sql.csv
 outfile=tmp/compare.$PREFIX.$SITE.$ISP
 
-cat $file1 | sed -e 's/raw_download_rate/'$SITE1.$ISP1'/g' | sort -n > $tmp1
-cat $file2 | sed -e 's/raw_download_rate/'$SITE2.$ISP2'/g' | sort -n > $tmp2
+cat $file1 | sed -e 's/raw_download_rate/'${SITE1}_to_$ISP1'/g' | sort -n > $tmp1
+cat $file2 | sed -e 's/raw_download_rate/'${SITE2}_to_$ISP2'/g' | sort -n > $tmp2
 
 ./queryview.py --merge $tmp1 \
                --merge $tmp2 \
@@ -45,15 +51,17 @@ cat $file2 | sed -e 's/raw_download_rate/'$SITE2.$ISP2'/g' | sort -n > $tmp2
 sort -n $outfile.csv > $outfile.2.csv 
 
 ./queryview.py --timestamp day_timestamp \
-            -l $SITE1.$ISP1 -C $COLOR1 \
-            -l $SITE2.$ISP2 -C $COLOR2 \
+            -l ${SITE1}_to_$ISP1 -C $COLOR1 \
+            -l ${SITE2}_to_$ISP2 -C $COLOR2 \
             --csv $outfile.2.csv \
             --between 20130902T06:00,20130903T06:00 \
             --pivot   20130902T06:00 \
             --output graphs/compare.$PREFIX.$SITE.$ISP.png \
+            -M '.' \
+            -M '.' \
             --datefmt "%H" \
             --offset 72000 \
-            --title "${ISP} Download Rates" \
+            --title "$PROVIDER to $ISP Downloads" \
             --ylabel "Mbps" \
             --xlabel "Eastern Time (UTC-4)" \
             --ymax 68
